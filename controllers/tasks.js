97ee8654590034d21a1bb7826a743059a2be3da7
Sievaes <146020864+Sievaes@users.noreply.github.com>
2025-03-10 20:40:16 +0200

    update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,7 +6,6 @@ const Task = require("../models/task")
 tasksRouter.get("/", async (req, res, next) => {
   try {
     const tasks = await Task.find({})
-
     res.status(200).json(tasks)
   } catch (error) {
     next(error)
@@ -16,7 +15,6 @@ tasksRouter.get("/", async (req, res, next) => {
 //POST new task
 tasksRouter.post("/", async (req, res, next) => {
   try {
-    console.log(req.body)
     const newTask = new Task({
       task: req.body.task,
       description: req.body.description,
@@ -56,38 +54,64 @@ tasksRouter.delete("/:id", async (req, res, next) => {
   }
 })
 
-//DELETE delete subtask
-tasksRouter.put("/:taskId/subtasks/:subTaskId", async (req, res, next) => {
-  try {
-    const { taskId, subTaskId } = req.params
-    const response = await Task.findByIdAndUpdate(
-      taskId,
-      { $pull: { subTasks: { _id: subTaskId } } },
-      { new: true }
-    )
-    if (response) {
-      const updatedTask = await Task.findById(taskId)
-      console.log(updatedTask)
-      res.status(200).json(updatedTask)
-    } else {
-      res.status(404).json({ message: "Subtask not found" })
-    }
-  } catch (error) {
-    next(error)
-  }
-})
+//PUT delete subtask
+tasksRouter.put(
+  "/:taskId/subtasks/:subTaskId/remove",
+  async (req, res, next) => {
+    try {
+      const { taskId, subTaskId } = req.params
+      const response = await Task.findByIdAndUpdate(
+        taskId,
+        { $pull: { subTasks: { _id: subTaskId } } },
+        { new: true }
+      )
 
-//PUT update task
+      if (response) {
+        const updatedTask = await Task.findById(taskId)
+        res.status(200).json(updatedTask)
+      } else {
+        res.status(404).json({ message: "Subtask not found" })
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+//PUT change task to completed
 tasksRouter.put("/:id", async (req, res, next) => {
   try {
     const id = req.params.id
     const body = req.body
-
     const updatedTask = await Task.findByIdAndUpdate(id, body, { new: true })
-
     res.status(200).json(updatedTask)
   } catch (error) {
     next(error)
   }
 })
+
+//PUT change subtask to completed
+tasksRouter.put(
+  "/:taskId/subtasks/:subTaskId/completed",
+  async (req, res, next) => {
+    try {
+      const { taskId, subTaskId } = req.params
+      const response = await Task.findOneAndUpdate(
+        { _id: taskId, "subTasks._id": subTaskId },
+        { $set: { "subTasks.$.completed": true } },
+        { new: true }
+      )
+
+      if (response) {
+        const updatedTask = await Task.findById(taskId)
+        res.status(200).json(updatedTask)
+      } else {
+        res.status(404).json({ message: "Subtask not found" })
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
 module.exports = tasksRouter
